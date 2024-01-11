cacf716cb0e7c9d05f1cfbdabb46f80370c203f0
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-01-11 15:23:47 +0600

    project setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-sky-600 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
 };
 
 export default App;
