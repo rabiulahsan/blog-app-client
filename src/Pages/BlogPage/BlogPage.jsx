e9bdab892c21304e149708f6d990f5bee74c5921
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-01-14 19:17:40 +0600

    create banner of blogs page

--- src/Pages/BlogPage/BlogPage.jsx
+++ src/Pages/BlogPage/BlogPage.jsx
@@ -1,5 +1,19 @@
+import SectionTitle from "../../Components/SectionTitle/SectionTitle";
+import PageBanner from "../../Shared/PageBanner/PageBanner";
+import Navbar from "../Home/Navbar/Navbar";
+
 const BlogPage = () => {
-  return <div></div>;
+  const details = {
+    image: "https://sb.ecobnb.net/app/uploads/sites/3/2022/04/copertina-1.jpg",
+    name: "Blogs",
+  };
+  return (
+    <div>
+      <Navbar></Navbar>
+      <PageBanner details={details}></PageBanner>
+      <SectionTitle heading="Know about your Destination"></SectionTitle>
+    </div>
+  );
 };
 
 export default BlogPage;
