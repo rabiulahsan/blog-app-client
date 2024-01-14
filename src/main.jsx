ae2caa1b7bfecf42207dc47af32fdf2a82cba862
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-01-14 21:50:05 +0600

    create skelton card

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <SkeletonTheme baseColor="#313131" highlightColor="#525252">
+      <RouterProvider router={router}></RouterProvider>
+    </SkeletonTheme>
   </React.StrictMode>
 );
