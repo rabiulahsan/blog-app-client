8cce1f53f8f8501cf0bc09724056dac2cfda43fe
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-01-14 18:30:10 +0600

    create a component for button hove effect

---
+++ src/Components/HoverOverlayButton/HoverOverlayButton.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import "./btn.css";
+
+const HoverOverlayButton = ({ btnName }) => {
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+  const handleMouseMove = (e) => {
+    // const x = e.pageX - e.currentTarget.offsetLeft;
+    // const y = e.pageY - e.currentTarget.offsetTop;
+
+    //it is used for specify the target element
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setCoordinates({ x, y });
+
+    // console.log(e);
+  };
+  const buttonStyles = {
+    "--before-top": `${coordinates.y}px`,
+    "--before-left": `${coordinates.x}px`,
+  };
+  console.log(buttonStyles);
+
+  return (
+    <button
+      onMouseMove={(e) => handleMouseMove(e)}
+      style={buttonStyles}
+      className="  button px-6 py-2 font-semibold text-lg rounded text-white inline-flex relative overflow-hidden bg-orange-500 hover:text-orange-600"
+    >
+      <span className="relative z-30">{btnName}</span>
+    </button>
+  );
+};
+
+export default HoverOverlayButton;
