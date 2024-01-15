230476593acc92aee9ff7a44b9759c87c825147e
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-01-15 14:49:04 +0600

    log in via google completed

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,46 @@
 import { Player } from "@lottiefiles/react-lottie-player";
 import { FcGoogle } from "react-icons/fc";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../../Hooks/UseAuth/UseAuth";
 
 const LoginPage = () => {
+  const { user, googleLogin } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
+
+  const handleGoogleSignIn = () => {
+    if (user) {
+      alert("At first logout");
+      return;
+    } else {
+      googleLogin()
+        .then((result) => {
+          const loggedInUser = result.user;
+          console.log(loggedInUser);
+          //   const saveUser = {
+          //     name: loggedInUser.displayName,
+          //     email: loggedInUser.email,
+          //     role: "user",
+          //   };
+          //   fetch("https://skill-builder-server.vercel.app/users", {
+          //     method: "POST",
+          //     headers: {
+          //       "content-type": "application/json",
+          //     },
+          //     body: JSON.stringify(saveUser),
+          //   })
+          //     .then((res) => res.json())
+          //     .then(() => {
+          //       navigate(from, { replace: true });
+          //     });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-sky-100">
       <div className="bg-white border border-gray-400 p-5">
@@ -13,7 +52,10 @@ const LoginPage = () => {
             src="/signup.json"
           ></Player>
         </div>
-        <div className=" border border-blue-500 my-8  flex text-center text-white cursor-pointer font-semibold">
+        <div
+          onClick={handleGoogleSignIn}
+          className=" border border-blue-500 my-8  flex text-center text-white cursor-pointer font-semibold"
+        >
           <span className="text-2xl p-2">
             <FcGoogle></FcGoogle>
           </span>
