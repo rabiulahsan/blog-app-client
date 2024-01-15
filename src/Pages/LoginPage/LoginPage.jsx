cb113cc95aeff582e381c7dfef77986433096640
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-01-15 14:34:53 +0600

    log in page design completed

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,27 @@
 import { Player } from "@lottiefiles/react-lottie-player";
+import { FcGoogle } from "react-icons/fc";
 
 const LoginPage = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-sky-100">
-      <div className="">
-        <Player className="h-[200px]" autoplay loop src="/signup.json"></Player>
+      <div className="bg-white border border-gray-400 p-5">
+        <div className="">
+          <Player
+            className="h-[200px]"
+            autoplay
+            loop
+            src="/signup.json"
+          ></Player>
+        </div>
+        <div className=" border border-blue-500 my-8  flex text-center text-white cursor-pointer font-semibold">
+          <span className="text-2xl p-2">
+            <FcGoogle></FcGoogle>
+          </span>
+          <span className="bg-blue-500 w-full p-2 hover:bg-blue-600">
+            Sign in With Google
+          </span>
+        </div>
       </div>
-      <div className="">this is div</div>
     </div>
   );
 };
