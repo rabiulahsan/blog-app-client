fd442efe70c4c0468007ec3443e0c73b442e271e
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-01-19 14:29:03 +0600

    blogs card updated for my blogs

--- src/Shared/BlogsCard/BlogsCard.jsx
+++ src/Shared/BlogsCard/BlogsCard.jsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
 import HoverOverlayButton from "../../Components/HoverOverlayButton/HoverOverlayButton";
 import FadeAnimations from "../FadeAnimations/FadeAnimations";
+import useAuth from "../../Hooks/UseAuth/UseAuth";
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
 
 /* eslint-disable react/prop-types */
 
 const BlogsCard = ({ place }) => {
+  const { user } = useAuth();
   // console.log(place);
 
-  const { _id, image, placeName, location, description, category, country } =
-    place;
+  const {
+    _id,
+    email,
+    image,
+    placeName,
+    location,
+    description,
+    category,
+    country,
+  } = place;
+
   return (
     <FadeAnimations direction="up" once={true} delay={0.4} duration={0.4}>
       <div className=" mb-[5%]">
@@ -26,10 +39,27 @@ const BlogsCard = ({ place }) => {
           <p>{country}</p>
         </div>
         <p className="text-center">{description.slice(0, 120) + "..."}</p>
-        <div className="flex justify-center items-center mt-3">
+        <div className="flex justify-center items-center mt-3 gap-x-4">
           <Link to={`/${_id}`}>
             <HoverOverlayButton btnName="Read More"></HoverOverlayButton>
           </Link>
+
+          {user?.email === email ? (
+            <>
+              <span className="bg-[#322F4B] text-white text-lg font-semibold p-[11px] rounded cursor-pointer">
+                <Link>
+                  <FaEdit></FaEdit>
+                </Link>
+              </span>
+              <span className="bg-[#322F4B] text-white text-lg font-semibold p-[11px] rounded cursor-pointer">
+                <Link>
+                  <MdDelete></MdDelete>
+                </Link>
+              </span>
+            </>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </FadeAnimations>
