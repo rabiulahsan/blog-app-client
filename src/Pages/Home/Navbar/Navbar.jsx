f22112f4be5fbe7cc13b30fb5f9b0c37682a4ac6
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-01-21 20:20:18 +0600

    isloading added on navbar

--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { Link, useNavigate } from "react-router-dom";
 import ActiveLink from "../../../Components/ActiveLink/ActiveLink";
@@ -6,6 +7,7 @@ import HoverOverlayButton from "../../../Components/HoverOverlayButton/HoverOver
 import useAuth from "../../../Hooks/UseAuth/UseAuth";
 import { FaSearch } from "react-icons/fa";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const Navbar = () => {
   const { logOut, user } = useAuth();
@@ -50,93 +52,110 @@ const Navbar = () => {
   };
   // console.log(buttonStyles);
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("places.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setIsLoading(false);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
-    <div className="sticky top-0 left-0 z-50">
-      <div className="flex justify-between items-center w-full absolute top-0 left-0  bg-[rgba(0,0,0,0.6)] py-3 px-[5%] text-white">
-        <Link to="/">
-          <p className="text-3xl text-orange-500 font-extrabold">Explorer</p>
-        </Link>
-        <div className="">
-          <ul className="flex text-[17px] font-semibold">
-            <li className="navlink-hover">
-              <span>
-                <ActiveLink to="/blogs">Blogs</ActiveLink>
-              </span>
-            </li>
-            <li className="navlink-hover">
-              <span>
-                <ActiveLink to="/countries">Countries</ActiveLink>
-              </span>
-            </li>
-            <li className="navlink-hover">
-              <span>
-                <ActiveLink to="/categories">Categories</ActiveLink>
-              </span>
-            </li>
-
-            {user ? (
-              <>
-                <li className="navlink-hover">
-                  <span>
-                    <ActiveLink to="/favourites">Favourites</ActiveLink>
-                  </span>
-                </li>
-                <li className="navlink-hover">
-                  <span>
-                    <ActiveLink to="/write">Write</ActiveLink>
-                  </span>
-                </li>
-                <li className="navlink-hover">
-                  <span>
-                    <ActiveLink to="/myblogs">My Blogs</ActiveLink>
-                  </span>
-                </li>
-              </>
-            ) : (
-              ""
-            )}
-          </ul>
-        </div>
-        <div className="flex items-center gap-x-4">
-          <div className="flex items-center">
-            <input
-              type="text"
-              placeholder="search"
-              className=" border border-orange-500 px-3 py-[7px] bg-transparent rounded-l focus:outline-none "
-              value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
-            />
-            <button
-              onMouseMove={(e) => handleMouseMove(e)}
-              style={buttonStyles}
-              className="  button p-3  font-semibold text-base rounded-r text-white inline-flex relative overflow-hidden bg-orange-500 "
-              onClick={handleSearch}
-            >
-              <span className="z-20">
-                <FaSearch></FaSearch>
-              </span>
-            </button>
-          </div>
-
-          {user ? (
-            <>
-              <img
-                className="h-[40px] rounded-[50%]"
-                src={user?.photoURL}
-                alt=""
-              />
-              <p onClick={handleLogOut}>
-                <HoverOverlayButton btnName="Logout"></HoverOverlayButton>
+    <>
+      {!isLoading && (
+        <div className="sticky top-0 left-0 z-50">
+          <div className="flex justify-between items-center w-full absolute top-0 left-0  bg-[rgba(0,0,0,0.6)] py-3 px-[5%] text-white">
+            <Link to="/">
+              <p className="text-3xl text-orange-500 font-extrabold">
+                Explorer
               </p>
-            </>
-          ) : (
-            <Link to="/login">
-              <HoverOverlayButton btnName="Join Us"></HoverOverlayButton>
             </Link>
-          )}
+            <div className="">
+              <ul className="flex text-[17px] font-semibold">
+                <li className="navlink-hover">
+                  <span>
+                    <ActiveLink to="/blogs">Blogs</ActiveLink>
+                  </span>
+                </li>
+                <li className="navlink-hover">
+                  <span>
+                    <ActiveLink to="/countries">Countries</ActiveLink>
+                  </span>
+                </li>
+                <li className="navlink-hover">
+                  <span>
+                    <ActiveLink to="/categories">Categories</ActiveLink>
+                  </span>
+                </li>
+
+                {user ? (
+                  <>
+                    <li className="navlink-hover">
+                      <span>
+                        <ActiveLink to="/favourites">Favourites</ActiveLink>
+                      </span>
+                    </li>
+                    <li className="navlink-hover">
+                      <span>
+                        <ActiveLink to="/write">Write</ActiveLink>
+                      </span>
+                    </li>
+                    <li className="navlink-hover">
+                      <span>
+                        <ActiveLink to="/myblogs">My Blogs</ActiveLink>
+                      </span>
+                    </li>
+                  </>
+                ) : (
+                  ""
+                )}
+              </ul>
+            </div>
+            <div className="flex items-center gap-x-4">
+              <div className="flex items-center">
+                <input
+                  type="text"
+                  placeholder="search"
+                  className=" border border-orange-500 px-3 py-[7px] bg-transparent rounded-l focus:outline-none "
+                  value={searchValue}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                />
+                <button
+                  onMouseMove={(e) => handleMouseMove(e)}
+                  style={buttonStyles}
+                  className="  button p-3  font-semibold text-base rounded-r text-white inline-flex relative overflow-hidden bg-orange-500 "
+                  onClick={handleSearch}
+                >
+                  <span className="z-20">
+                    <FaSearch></FaSearch>
+                  </span>
+                </button>
+              </div>
+
+              {user ? (
+                <>
+                  <img
+                    className="h-[40px] rounded-[50%]"
+                    src={user?.photoURL}
+                    alt=""
+                  />
+                  <p onClick={handleLogOut}>
+                    <HoverOverlayButton btnName="Logout"></HoverOverlayButton>
+                  </p>
+                </>
+              ) : (
+                <Link to="/login">
+                  <HoverOverlayButton btnName="Join Us"></HoverOverlayButton>
+                </Link>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
